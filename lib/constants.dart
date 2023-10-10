9aaffc974ec0a1c4b76d9a8976aa0761682d7435
MinkyeongSung <wildlegion59@gmail.com>
2023-10-10 14:50:03 +0900

    쇼핑카트앱

---
+++ lib/constants.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+const kPrimaryColor = Color(0xFFeeeeee); // 앱 브랜드 색
+const kSecondaryColor = Color(0xFFc6c6c6); // 앱 브랜드 색
+const kAccentColor = Color(0xFFff7643); // 앱 브랜드 색
